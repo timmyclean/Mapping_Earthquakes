43e15274eefbebdbe30cd10dcf9f8c24a6fef43c
Tim McLean <timmyclean@yahoo.ca>
2021-03-29 12:48:55 -0400

    Final with Dark mode

--- EarthQuake_Challenge/Static/js/challenge_logic.js
+++ EarthQuake_Challenge/Static/js/challenge_logic.js
@@ -15,7 +15,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
-let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       accessToken: API_KEY
@@ -32,7 +32,7 @@ let map = L.map('mapid', {
 let baseMaps = {
   "Streets": streets,
   "Satellite": satelliteStreets,
-  "Outdoors": outdoors
+  "Dark": dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
